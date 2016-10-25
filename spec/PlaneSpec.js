b528838a4b677ba921da623f3b9a8e173cfe2bda
Amaal Ali <amq.ali@gmail.com>
2016-10-25 10:27:32 +0100

    Plane class created.

---
+++ spec/PlaneSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Plane', function(){
+  var plane;
+
+  beforeEach(function() {
+    plane = new Plane();
+  });
+
+  it('can land at airport', function(){
+    expect(plane.land).not.toBeUndefined();
+  });
+});
--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,11 +1,6 @@
-describe("airport", function() {
+'use strict';
 
-	describe('Land', function() {
-		it('instructs plane to', function() {
-			var airport = new Airport();
-			var plane = new ();
-			airport.land(plane);
-			expect(airport.planes()).toContain(plane);
-		});
-	});
+
+describe("Airport", function() {
+
 })
